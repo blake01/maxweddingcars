a35986b8319a1c41295ce04869db3b19ed3f5611
Blake Hemingway <blakehemingway@gmail.com>
2014-08-10 17:39:20 +0100

    Missing File from prev. commit

---
+++ locations.php
@@ -0,0 +1,131 @@
+
+<html lang="en">
+<head>
+    <meta content="text/html; charset="utf-8">
+    <title>Chauffeur Driven Wedding Cars Leeds | Ring 01924 370485 For Chauffeur Driven Car Hire Leeds</title>
+    <meta content="Wedding Cars Leeds, Wedding Car Hire Leeds, Vintage Wedding Cars Leeds, Chauffeur Driven Car Hire Leeds, Chauffeur Driven Wedding Cars Leeds, Vintage Car Hire Leeds, Classic Car Hire Leeds, Wedding Limos Leeds, Wedding Limo Hire Leeds" name="keywords">
+    <meta content="
+Wedding Cars Leeds, Wedding Car Hire Leeds, Vintage Wedding Cars Leeds, Chauffeur Driven Car Hire Leeds, Chauffeur Driven Wedding Cars Leeds, Vintage Car Hire Leeds, Classic Car Hire Leeds, Wedding Limos Leeds, Wedding Limo Hire Leeds" name="description">
+    <!-- Wedding Cars Leeds, Wedding Car Hire Leeds, Vintage Wedding Cars Leeds, Chauffeur Driven Car Hire Leeds, Chauffeur Driven Wedding Cars Leeds, Vintage Car Hire Leeds, Classic Car Hire Leeds, Wedding Limos Leeds, Wedding Limo Hire Leeds -->
+    <meta content="index,follow" name="robots">
+    <meta content="7 days" name="revisit-after">
+    <meta content="GB-LDS" name="geo.region">
+    <meta content="Leeds" name="geo.placename">
+    <meta content="53.801279;-1.548567" name="geo.position">
+    <meta content="53.801279, -1.548567" name="ICBM">
+    <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link href="assets/style.css" rel="stylesheet" type="text/css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript"></script>
+    
+</head>
+
+<body class="oneColFixCtr">
+    <div id="fb-root"></div>
+    <script>
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+    <div class="centerer" id="container">
+        <div id="header">
+            <img alt="Max Hemingway Wedding Car Hire" src="Header.jpg">
+
+            <div id="headerhome">
+                <a href="/">Home</a>
+            </div>
+
+            <div id="headerenq">
+                <a href="contact">Enquire</a>
+            </div>
+
+            <div id="headerloc">
+                <a href="locations">Locations</a>
+            </div>
+        </div>
+
+        <div class="sidebar">
+            <h4>Click on images for further details</h4>
+
+            <p><a href="vintage"><img alt="vintage style tourer" src="Sidebar/beaufordsidebar.jpg"></a></p>
+
+            <p>Vintage Style Tourers</p>
+
+            <p><a href="seven_seat_mercedes"><img alt="mercedes wedding car" src="Sidebar/stretchmercsidebar.jpg"></a></p>
+
+            <p>Mercedes-Benz Seven Passenger Limousine</p>
+
+            <p><a href="mercedes"><img alt="mercedes wedding car" src="Sidebar/mercsidebar.jpg"></a></p>
+
+            <p>Mercedes-Benz</p>
+
+            <div class="fb-like-box" data-colorscheme="light" data-header="true" data-href=
+            "https://www.facebook.com/pages/Maxweddingcars/259756950891843?fref=ts" data-show-border="true" data-show-faces="true" data-stream="false"
+            data-width="250"></div>
+        </div>
+
+        <div class="text">
+            
+<h1 id="bighead">Wedding Car Hire in Yorkshire</h1>
+<p>We are based in <strong>Wakefield</strong> and believe that we offer the finest <strong>Wedding Car Hire service in Yorkshire</strong>.We serve all towns and cities therein, including, but not limited to:</p>
+<h2 class="mainhead">Wedding Car Hire Leeds</h2>
+<p>Max Hemingway Chauffeur Services have been providing <strong>Wedding Car Hire in Leeds</strong> since 1983, and can offer both modern vehicles such as our Mercedes Benz, or vintage style cars such as our 1930s style tourers, to ensure that you arrive in style on your special day in <strong>Leeds</strong> and surrounding towns/cities including <strong>Bradford, Morley, Keighley, Ilkley, Otley and Skipton.</strong></p>
+<h2 class="mainhead">Wedding Car Hire Wakefield</h2>
+<p>Over the last 28 years, we have built up a formidable reputation for reliablity and levels of service in our home city of <strong>Wakefield</strong>. We regularly chauffeur brides, grooms and civil partners to their ceremonies in <strong>Wakefield </strong>and the Five Towns of <strong>Pontefract, Castleford, Knottingley, Featherstone and Normanton</strong>, and also further east to <strong>Goole and Selby</strong>.</p>
+<h2 class="mainhead">Wedding Car Hire Huddersfield </h2>
+<p>Our exceptional cars are available for <strong>Wedding Car Hire in Huddersfield</strong> and the surrounding area, and offer  unrivalled value for money to those seeking<strong> Wedding Car Hire in Huddersfield. </strong>Our outstanding vehicles can be trimmed to coordinate with your own personal wedding theme to ensure that you arrive in style to your wedding, be it in <strong>Halifax, Holmfirth, Sowerby Bridge, Brighouse, Cleckheaton, Batley, Dewsbury, Heckmondwike, Liversedge or Mirfield.</strong></p>
+<h2 class="mainhead">Wedding Car Hire York</h2>
+<p>We also have many years experience of serving <strong>North Yorkshire</strong>, and can provide you with a bespoke quote for <strong>Wedding Car Hire in York</strong> if you contact us on the telephone number above or via the 'enquire' link. We provide chauffeur services in the York area, including the towns of <strong>Harrogate, Wetherby, Knaresborough and Tadcaster.</strong></p>
+<h2 class="mainhead">Wedding Car Hire Sheffield</h2>
+<p>Our reputation is no less formidable in <strong>South Yorkshire</strong>, where we can provide affordable <strong>Wedding Car Hire in Sheffield, Rotherham, Barnsley and Doncaster.</strong></p>
+<p>Note that the list above is not exhaustive; please contact us for a quote for any location in the UK.</p>
+
+        </div><!-- end #container -->
+    </div>
+
+    <div class="centerer">
+        <footer class="footer">
+            Max Hemingway<br>
+            01924 370485<br>
+            Bradford Road, Wakefield, West Yorkshire, WF12AS<br>
+            Email: <a class="style4" href=
+            "mailto:max@maxweddingcars.co.uk?subject=Enquiry%20R.E.%20Chauffeur%20Driven%20Car%20Hire">max@maxweddingcars.co.uk</a><br>
+            Site Design &copy; 2014 Blake Hemingway<br>
+            <span class="footer1">Serving all areas of Yorkshire: View <a href="locations.php">Our Locations</a></span>
+
+            <div style="text-align: center">
+                <br />
+
+                <h1 style="text-align: center; font-size: 9px; color:#666; font-family: Arial, Helvetica, sans-serif;">Wedding Cars Leeds ~ Wedding Car Hire
+                Leeds ~ Vintage Wedding Cars Leeds ~ Chauffeur Driven Car Hire Leeds ~ Chauffeur Driven Wedding Cars Leeds ~ Vintage Car Hire Leeds ~ Classic
+                Car Hire Leeds<br>
+                Wedding Limos Leeds ~ Wedding Limo Hire Leeds</h1>
+
+                <p style="text-align: center; font-size: 9px; font-family: Arial, Helvetica, sans-serif;"><a href="http://www.uniteldirect.co.uk/" style=
+                "font-size: 9px; color:#666;" target="_blank" title="SEO By Unitel Direct">SEO By Unitel Direct</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href=
+                "http://www.businessinternetfinder.com/" style="font-size: 9px; color:#666;" target="_blank" title="Business Internet Finder">Business Internet
+                Finder</a><a href="http://www.thetradefinder.co.uk/" style="font-size: 9px; color:#666;" target="_blank" title="Trade Finder">&nbsp;And Trade
+                Finder</a></p>
+            </div>
+        </footer>
+    </div><!-- Start of StatCounter Code -->
+    <script type="text/javascript">
+        var sc_project=3538675;
+        var sc_invisible=1;
+        var sc_security="0d87c190";
+    </script> 
+    <script src="http://www.statcounter.com/counter/counter_xhtml.js" type="text/javascript"></script>
+    <noscript>
+        <div class="statcounter">
+            <a class="statcounter" href="http://statcounter.com/tumblr/" title="tumblr hit tracking tool"><img alt="tumblr hit tracking tool" class="statcounter"
+            src="http://c.statcounter.com/3538675/0/0d87c190/1/"></a>
+        </div>
+    </noscript> <!-- End of StatCounter Code -->
+</body>
+</html>
