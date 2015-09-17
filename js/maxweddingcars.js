94951f742cee95eac55452bce4b6f3fb8157d726
Blake Hemingway <blakehemingway@gmail.com>
2015-09-17 07:18:22 +0100

    iOS Double Tap issue fix

--- js/maxweddingcars.js
+++ js/maxweddingcars.js
@@ -1,3 +1,14 @@
-$("#mobile-nav-toggle").click(function() {
-  $("nav").slideToggle();
+$(document).ready(function() {
+
+  $("#mobile-nav-toggle").click(function() {
+    $("nav").slideToggle();
+  });
+
+  // Overcome iOS double tap issue on links with hover states
+  $('a').on('click touchend', function(e) {
+    var el = $(this);
+    var link = el.attr('href');
+    window.location = link;
+  });
+
 });
