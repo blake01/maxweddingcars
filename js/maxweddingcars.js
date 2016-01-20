a1f1c77232bcbaf95846de89881bece6995087aa
Blake Hemingway <blakehemingway@gmail.com>
2016-01-20 22:11:45 +0000

    Remove redundant code
    Also test post-receive hook.

--- js/maxweddingcars.js
+++ js/maxweddingcars.js
@@ -4,13 +4,6 @@ $(document).ready(function() {
     $("nav").slideToggle();
   });
 
-  // Overcome iOS double tap issue on links with hover states
-  $("a").on("click touchend", function(e) {
-    var el = $(this);
-    var link = el.attr("href");
-    window.location = link;
-  });
-
   var isHome = ! $("#owl-example").hasClass("car-carousel");
 
   $("#owl-example").owlCarousel({
